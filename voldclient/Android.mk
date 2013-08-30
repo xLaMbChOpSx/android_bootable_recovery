781f7815bfe1c1187b6baf4b3e3ef8a4254bc098
xLaMbChOpSx <ryanskungfu@me.com>
2013-08-30 22:35:19 +1000

    Linaro toolchain 4.9 Build Fix
    Change-Id: I9a52e64f5ff38d018c63156882552c102764a043

--- voldclient/Android.mk
+++ voldclient/Android.mk
@@ -1,9 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 
+common_cflags += -Werror \
+	-Wno-error=unused-parameter
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := libvoldclient
 LOCAL_SRC_FILES := commands.c dispatcher.c event_loop.c
-LOCAL_CFLAGS := -DMINIVOLD -Werror
+LOCAL_CFLAGS := $(common_cflags) -DMINIVOLD
+
 LOCAL_C_INCLUDES :=         	\
     bootable/recovery       	\
     system/core/fs_mgr/include	\
